0230b9ddfa62a44669a44d507c9957e06ea8809f
Eleonora Reuk <106328644+EleonoraReuk@users.noreply.github.com>
2026-03-18 18:37:37 +0700

    Update part2.cpp

--- part2.cpp
+++ part2.cpp
@@ -1,24 +1,26 @@
 #include <iostream>
 
-void create_array();
+double* create_array(int& array_size);
 
 int main()
 {
-	create_array();
-
-	return EXIT_SUCCESS;
-}
-
-void create_array()
-{
-	int array_size{};
-	std::cout << "Введите размер массива: ";
-	std::cin >> array_size;
-	double* int_array = new double[array_size] {};
+    int array_size{};
+	double* int_array = create_array(array_size);
+	
 	std::cout << "Массив: ";
 	for (int i = 0; i < array_size; ++i)
 	{
 		std::cout << int_array[i] << " ";
 	}
-	delete[] int_array;
+	
+    delete[] int_array;
+	return EXIT_SUCCESS;
+}
+
+double* create_array(int& array_size)
+{
+	std::cout << "Введите размер массива: ";
+	std::cin >> array_size;
+	double* int_array = new double[array_size] {};
+	return int_array;
 }
